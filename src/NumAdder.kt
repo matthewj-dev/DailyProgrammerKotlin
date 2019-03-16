d6d10013258774bc3082658f8630f4bb48643ba2
Matthew Jackson <mjacks36@outlook.com>
2019-03-16 17:03:24 -0500

    First Commit

---
+++ src/NumAdder.kt
@@ -0,0 +1,33 @@
+//Programming challenge from here:
+// https://www.reddit.com/r/dailyprogrammer/comments/aphavc/20190211_challenge_375_easy_print_a_new_number_by/?ref=share&ref_source=link
+
+fun adder(x: Int): Int {
+    val result: Int
+    var num: Int = x
+    val parts: MutableList<Int> = ArrayList()
+    while (num > 0) {
+        parts.add((num % 10) + 1)
+        num /= 10
+    }
+
+    result = sumInPlace(parts)
+
+    return result
+}
+
+fun sumInPlace(lst: MutableList<Int>): Int {
+    var sum = 0
+    var place = 0
+    for (item in lst){
+
+        sum += item * (Math.pow((10).toDouble(), place.toDouble())).toInt()
+
+        if (item < 10) place++
+        else place += 2
+    }
+    return sum
+}
+
+fun main() {
+    print(adder(1029))
+}
